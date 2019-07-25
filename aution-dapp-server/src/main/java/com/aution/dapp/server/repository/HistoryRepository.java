6d8033ca96be83b007de04eb2245d05ebda09eeb
li.dongming <li.dongming@cesgroup.com.cn>
2019-07-25 17:53:36 +0800

    add userinfo

--- aution-dapp-server/src/main/java/com/aution/dapp/server/repository/HistoryRepository.java
+++ aution-dapp-server/src/main/java/com/aution/dapp/server/repository/HistoryRepository.java
@@ -21,26 +21,26 @@ import org.springframework.web.bind.annotation.RequestBody;
 @Mapper
 public interface HistoryRepository extends PlatformMybatisRepository<History> {
 
-  @Select("select trade_no,goods_id,user_id,bid_price,bid_time,temp from t_history where temp = '1'")
+  @Select("select trade_no,goods_id,h.user_id,bid_price,bid_time,temp,avatar,user_name,user_phone from t_history h left join t_user u on h.user_id=u.user_id  where temp = '1'")
   List<History> findAllHistory();
 	
-  @Select("select trade_no,goods_id,user_id,bid_price,bid_time,temp from t_history where temp = '1' and user_id = #{userId} and goods_id = #{gId} order by bid_price DESC")	
+  @Select("select trade_no,goods_id,h.user_id,bid_price,bid_time,temp,avatar,user_name,user_phone from t_history h LEFT JOIN t_user u on h.user_id=u.user_id where temp = '1' and h.user_id = #{userId} and goods_id = #{gId} order by bid_price DESC")
   List<History> findHistoryByUserIdAndGoodsId(@Param("userId")String userId,@Param("gId")String gId,Pageable pageable);
   
-  @Select("select trade_no,goods_id,user_id,bid_price,bid_time,temp from t_history where temp = '1' and user_id = #{userId}")	
+  @Select("select trade_no,goods_id,h.user_id,bid_price,bid_time,temp,avatar,user_name,user_phone from t_history h LEFT JOIN t_user u on h.user_id=u.user_id where temp = '1' and h.user_id = #{userId}")
   List<History> findHistoryByUserId(@Param("userId")String userId,Pageable pageable);
   
-  @Select("select trade_no,goods_id,user_id,bid_price,bid_time,temp from t_history where temp = '1' and goods_id = #{gId} ")	
+  @Select("select trade_no,goods_id,h.user_id,bid_price,bid_time,temp,avatar,user_name,user_phone from t_history h LEFT JOIN t_user u on h.user_id=u.user_id where temp = '1' and goods_id = #{gId} ")
   List<History> findHistoryByGoodsIdAndTimeSort(@Param("gId")String gId,Pageable pageable);
   
   
-  @Select("select trade_no,goods_id,user_id,max(bid_price)bid_price,bid_time,temp from t_history  where temp = '1' and goods_id = #{gId} group by user_id ")	
+  @Select("SELECT trade_no,goods_id,h.user_id,tt.bid_price,bid_time,temp,avatar,user_name,user_phone FROM t_history h LEFT JOIN t_user t ON h.user_id = t.user_id LEFT JOIN (SELECT user_id,max(bid_price) bid_price FROM t_history WHERE temp = '1' and goods_id = #{gId} GROUP BY user_id) tt ON tt.user_id=t.user_id")
   List<History> findHistoryByGoodsIdAndPriceSortAndGroupByUserId(@Param("gId")String gId,Pageable pageable);
   
   @Select("select MAX(bid_price)MaxPrice from t_history where temp = '1' and  goods_id = #{gId}")
   Double findMaxPriceByGid(@Param("gId")String gId);
   
-  @Select("select trade_no,t.goods_id,t.user_id,bid_price from t_history h ,(select user_id,goods_id from t_history where temp = '1' and trade_no = #{tradeNo})t where t.user_id = h.user_id and h.goods_id = t.goods_id order by bid_price DESC")
+  @Select("select trade_no,t.goods_id,t.user_id,bid_price,avatar,user_name,user_phone from t_history h ,t_user u,(select user_id,goods_id from t_history where temp = '1' and trade_no = #{tradeNo})t where t.user_id = t.user_id and  t.user_id = h.user_id and h.goods_id = t.goods_id order by bid_price DESC")
   List<History> findHistoryByTradeNoAndGidAndPriceSort(@Param("tradeNo")String tradeNo,Pageable pageable);
   
   
