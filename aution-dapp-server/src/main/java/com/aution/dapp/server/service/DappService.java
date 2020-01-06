dfc2e9938541a60666eaf5b4a18c086ba4e7f168
hewensheng <hews98@126.com>
2020-01-06 17:41:07 +0800

    完善下发任务

--- aution-dapp-server/src/main/java/com/aution/dapp/server/service/DappService.java
+++ aution-dapp-server/src/main/java/com/aution/dapp/server/service/DappService.java
@@ -489,8 +489,6 @@ public class DappService {
                     }
                 //交易不存在
                 } else if (data.get("status").equals("2")) {
-                    //businessRecord.setTradeNo(GenerateNoUtil.generateTradeNo());
-                    //hRepository.updateHistory(null,null,null,businessRecord.getTradeNo(),history.getTradeNo());
                     businessRecords.add(businessRecord);
                 }
             }
