286611fcd60a1981be2ae3297caa2112d7dc26d0
hewensheng <hews98@126.com>
2020-01-07 10:54:28 +0800

    test

--- aution-dapp-server/src/test/java/org/aution/dapp/server/AppTest.java
+++ aution-dapp-server/src/test/java/org/aution/dapp/server/AppTest.java
@@ -1,38 +1,43 @@
 package org.aution.dapp.server;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import com.aution.dapp.server.model.Goods;
+import com.aution.dapp.server.service.DappService;
+import com.aution.dapp.server.service.GoodsService;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.UUID;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest {
+
+    @Autowired
+    DappService dappService;
+    @Autowired
+    GoodsService goodsService;
+
+    @Test
+    public void createGoodsTest(){
+        Goods goods = new Goods();
+        goods.setGoodsId(UUID.randomUUID().toString());
+        goods.setEndTime(System.currentTimeMillis() + 50000L);
+        goods.setDetails("");
+        goods.setSellerId("");
+        goods.setStartPrice(1d);
+        goods.setTitle("");
+        goods.setType(1);
+        //goodsService.createGoods(goods,);
+    }
+    @Test
+    public void bidTest(){
+
+    }
+    @Test
+    public void getUserInfoTest(){
+
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
