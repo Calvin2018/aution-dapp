e917bd3bbec486febf2835d5b65da9894ba9cac5
liu.zhe@cesgroup.com.cn <liuzhegoodboy@aliyun>
2020-01-19 10:38:44 +0800

    最新

--- aution-dapp-web-vue-ui/src/main.js
+++ aution-dapp-web-vue-ui/src/main.js
@@ -4,6 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// import './view/ReleasePage/exif-js-master/exif'
+
 import '@/static/css/bass.css'
 
 import store from '@/store'
