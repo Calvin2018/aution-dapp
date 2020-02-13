28f11cccacc4dd8c529548e2c3b707c8cad135f6
hewensheng <hews98@126.com>
2020-02-13 12:15:37 +0800

    暂时屏蔽拦截器

--- aution-dapp-server/src/main/java/com/aution/dapp/server/config/MyInterceptorConfig.java
+++ aution-dapp-server/src/main/java/com/aution/dapp/server/config/MyInterceptorConfig.java
@@ -1,21 +1,32 @@
-package com.aution.dapp.server.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-
-@Configuration
-public class MyInterceptorConfig extends WebMvcConfigurationSupport {
-
-    @Bean
-    public MyInterceptor myInterceptor() {
-        return new MyInterceptor();
-    }
-
-    @Override
-    protected void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(myInterceptor()).addPathPatterns("/**");
-        super.addInterceptors(registry);
-    }
-}
+//package com.aution.dapp.server.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.ViewResolver;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+//import org.springframework.web.servlet.view.InternalResourceViewResolver;
+//
+//@Configuration
+//public class MyInterceptorConfig extends WebMvcConfigurationSupport {
+//
+//    @Bean
+//    public MyInterceptor myInterceptor() {
+//        return new MyInterceptor();
+//    }
+//
+//    @Bean
+//    public ViewResolver viewResolver(){
+//
+//        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+//        resolver.setCache(true);
+//        return resolver;
+//    }
+//
+//
+//    @Override
+//    protected void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(myInterceptor()).addPathPatterns("/api/goods/create","/api/order/bid");
+//        super.addInterceptors(registry);
+//    }
+//}
